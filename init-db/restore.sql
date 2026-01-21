3f00fef1feb27c73c455448a833da141003dc807
tin0206 <ngtrungtin02062005@gmail.com>
2026-01-21 14:36:03 +0700

    Automating restore db and run app

--- init-db/restore.sql
+++
@@ -1,7 +0,0 @@
-RESTORE DATABASE IGS
-FROM DISK = '/var/opt/mssql/backup/IGS-260116.bak'
-WITH
-MOVE 'IGS' TO '/var/opt/mssql/data/IGS.mdf',
-MOVE 'IGS_log' TO '/var/opt/mssql/data/IGS_log.ldf',
-REPLACE;
-GO
