a2e0472054e16ffc47a57b73455e70970b25378d
tin0206 <ngtrungtin02062005@gmail.com>
2026-01-27 11:55:29 +0700

    Uncomment day filter

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -32,12 +32,12 @@ document.addEventListener("DOMContentLoaded", async function () {
   sourceCodeSpan.textContent = PLANTCODE;
   destinationCodeSpan.textContent = LINE;
 
-  // if (dateFromInput) {
-  //   dateFromInput.value = yesterday.toISOString().split("T")[0];
-  // }
-  // if (dateToInput) {
-  //   dateToInput.value = tomorrow.toISOString().split("T")[0];
-  // }
+  if (dateFromInput) {
+    dateFromInput.value = yesterday.toISOString().split("T")[0];
+  }
+  if (dateToInput) {
+    dateToInput.value = tomorrow.toISOString().split("T")[0];
+  }
 
   await fetchStats();
   await fetchProductionOrders(1);
